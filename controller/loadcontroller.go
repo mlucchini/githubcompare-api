1c5e9221e2f9601ff5f65a5615386ff4026495da
mlucchini <marclucchini@gmail.com>
2016-03-08 16:54:55 +0000

    Solved issue with web files not found. Moved to a manual instance otherwise we only get 60 seconds to handle the request. Temporary for data loading

--- controller/loadcontroller.go
+++ controller/loadcontroller.go
@@ -10,6 +10,8 @@ import (
 	"github.com/mlucchini/github-compare-backend/model"
 	"github.com/mlucchini/github-compare-backend/network"
 	"github.com/mlucchini/github-compare-backend/service"
+	"google.golang.org/appengine/runtime"
+	"golang.org/x/net/context"
 )
 
 type LoadController struct {}
@@ -17,30 +19,32 @@ type LoadController struct {}
 func (self *LoadController) Update(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	bucket, file := params.ByName("bucket"), params.ByName("file")
 
-	ctx := appengine.NewContext(r)
-	loadService := service.LoadService{ ctx }
+	runtime.RunInBackground(appengine.NewContext(r), func(ctx context.Context) {
+		loadService := service.LoadService{ ctx }
 
-	reader, done, err := (&network.BucketHandler{ ctx }).Reader(bucket, file)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
-	defer done()
-
-	added := 0; err = nil
-	scanner := bufio.NewScanner(reader)
-	for scanner.Scan() && err == nil {
-		var entity model.RepositoryStarEvent
-		entity.Parse(scanner.Text())
-		_, err = loadService.Put(&entity)
-		added += 1
-		if added % 100 == 0 {
-			log.Infof(ctx, "Number of events added so far: %d", added)
+		reader, done, err := (&network.BucketHandler{ ctx }).Reader(bucket, file)
+		if err != nil {
+			log.Errorf(ctx, err.Error())
+			return
 		}
-	}
+		defer done()
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	fmt.Fprintf(w, "Number of events loaded: %d\n", added)
+		added := 0; err = nil
+		scanner := bufio.NewScanner(reader)
+		for scanner.Scan() && err == nil {
+			var entity model.RepositoryStarEvent
+			entity.Parse(scanner.Text())
+			_, err = loadService.Put(&entity)
+			if err != nil {
+				log.Errorf(ctx, err.Error())
+				return
+			}
+			added += 1
+			if added % 100 == 0 {
+				log.Infof(ctx, "Number of events added so far: %d", added)
+			}
+		}
+	})
+
+	fmt.Fprintf(w, "Launched loading in background")
 }
