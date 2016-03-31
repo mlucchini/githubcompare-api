e0be127c844a46886e082f2771834a2383e589e1
mlucchini <marclucchini@gmail.com>
2016-03-31 09:46:59 +0100

    Added Cache-Control for 3600 seconds

--- controller/statscontroller.go
+++ controller/statscontroller.go
@@ -13,7 +13,8 @@ type StatsController struct {}
 func (self *StatsController) Get(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
-	
+	w.Header().Set("Cache-Control", "public, max-age=3600, s-maxage=3600")
+
 	repositoryName := params.ByName("org") + "/" + params.ByName("repository")
 
 	ctx := appengine.NewContext(r)
